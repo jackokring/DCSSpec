323bce8b3e2f89f822ae5b2e1bebdea909f64c67
jackokring <jackokring@gmail.com>
2020-04-19 19:34:21 +0100

    init commit

---
+++ app/src/main/java/uk/co/kring/android/dcs/MainActivity.java
@@ -0,0 +1,13 @@
+package uk.co.kring.android.dcs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
