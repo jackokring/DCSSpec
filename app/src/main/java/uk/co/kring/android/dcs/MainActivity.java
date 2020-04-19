43f13fe3060a1957c403580ea787717e062bfe1b
jackokring <jackokring@gmail.com>
2020-04-19 19:57:00 +0100

    ok

--- app/src/main/java/uk/co/kring/android/dcs/MainActivity.java
+++ app/src/main/java/uk/co/kring/android/dcs/MainActivity.java
@@ -1,13 +1,52 @@
 package uk.co.kring.android.dcs;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.BaseAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyAdapter la = new MyAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((ListView)findViewById(R.id.list_view)).setAdapter(la);
+    }
+
+    private class MyAdapter extends BaseAdapter {
+
+        // override other abstract methods here
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup container) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
+            }
+
+            ((TextView) convertView.findViewById(R.id.text_view_id))
+                    .setText((String)getItem(position));
+            return convertView;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
     }
 }
