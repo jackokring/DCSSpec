3430123f16b05ef3429bad2d8558a1d0831cf6f3
jackokring <jackokring@gmail.com>
2020-06-17 17:04:30 +0100

    jndsk

--- app/src/main/java/uk/co/kring/android/dcs/jndsk/Recycle.java
+++ app/src/main/java/uk/co/kring/android/dcs/jndsk/Recycle.java
@@ -1,9 +1,14 @@
 package uk.co.kring.android.dcs.jndsk;
 
+import java.util.LinkedList;
+
 //A stacked object recycler to reduce GC overheads for an easier port to nds
+//This is just a work-alike
 public class Recycle {
 
     static Recycle s;
+    static LinkedList<Object> saves = new LinkedList<Object>();
+    static LinkedList<Integer> marks = new LinkedList<Integer>();
 
     public Recycle() {
         if(s != null) {
@@ -17,22 +22,29 @@ public class Recycle {
         return s;
     }
 
-    public void forget(int marks) {
+    public void forget(int markCount) {
         //TODO: go back number of marks in allocations using new
-        //and pack saves
+        for(int i = 0; i < markCount; ++i) {
+            int index = marks.removeLast();
+            while (index > saves.size()) {
+                saves.removeLast();
+            }
+        }
     }
 
     public void mark() {
         //TODO: mark a collection point
+        marks.add(saves.size());
     }
 
     public int save(Object object) {
         //TODO: make a note to save this and return index
-        return 0;
+        saves.add(object);
+        return saves.size() - 1;
     }
 
     public Object load(int index) {
         //TODO: return a new object pointer for a saved object
-        return null;
+        return saves.get(index);
     }
 }
